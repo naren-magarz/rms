1204e9380b5e3e94189c845fc07a5537ae4c1347
narenMagarZ <narenmagarz98@gmail.com>
2022-12-27 18:05:48 +0545

    change in staff collection schema

---
+++ backend/db/schema/staffcollectionschema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const staffCollectionSchema = mongoose.Schema({
+     'email' : {
+          type : String,
+          required : true
+     },
+     'level' : {
+          type : mongoose.Types.ObjectId,
+          required : true,
+          ref : 'level'
+     },
+     'faculty' : {
+          type : mongoose.Types.ObjectId,
+          required : true,
+          ref : 'faculty'
+      },
+      'program' : [
+          {
+               type : mongoose.Types.ObjectId,
+               required : true,
+               ref : 'program'
+          }
+      ],
+      'hod' : {
+          type : mongoose.Types.ObjectId,
+          ref : 'program',
+          default : null
+      }
+})
+
+module.exports.staffCollectionModel = mongoose.model('staffcollection',staffCollectionSchema)
