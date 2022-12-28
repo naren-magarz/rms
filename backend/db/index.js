250cb8fd4be789da7eca4ba21b32adfedf6ad055
sudipvass <sudipmallthakuri@gmail.com>
2022-12-28 11:28:45 +0545

    adding mongodb atlas cloud database

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,5 @@
 const {connect,connection} = require('mongoose')
-connect('mongodb://localhost/rms')
+connect('mongodb+srv://sudip:3rKqEKbWFRJoKc0p@cluster0.oey8rzt.mongodb.net/?retryWrites=true&w=majority')
 let db = connection
 db.on('error',(err)=>{
     console.error(err)
