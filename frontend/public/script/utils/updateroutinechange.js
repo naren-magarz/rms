708b2355cb849d3a41581c9bd9641e384438d87a
narenMagarZ <narenmagarz98@gmail.com>
2022-12-29 17:00:34 +0545

    updated frontend code

--- frontend/public/script/utils/updateroutinechange.js
+++ frontend/public/script/utils/updateroutinechange.js
@@ -1,24 +1,21 @@
 let timer 
 export function updateRoutineField(){
-     console.log(this.dataset)
+     const day = this.parentElement.parentElement.parentElement.dataset.day
      clearTimeout(timer)
      const {refid,oid,roomid} = this.dataset
      timer = setTimeout(() => {
-          fetch(`/api/room?context=${this.name}&id=${refid}&oid=${oid}&value=${this.value}&roomid=${roomid}`,{
+          fetch(`/api/updateroutine`,{
                'method' : 'PUT',
                'headers' : {
                     'content-type' : 'application/json;charset=utf-8'
                },
                'body' : JSON.stringify({
-                    'type' : this.dataset.type,
+                    'context' : this.name,
+                    'id' : refid,
+                    'oid' : oid,
                     'value' : this.value,
-                    'state' : this.dataset.state,
-                    'faculty' : window.localStorage.getItem('faculty'),
-                    'level' : window.localStorage.getItem('level'),
-                    'program' : this.dataset.program,
-                    'roomId' : this.name,
-                    'dayIndex' : this.dataset.dayindex,
-                    'index' : parseInt(this.dataset.index) - 1
+                    'roomId' : roomid,
+                    'day' : day.trim()
                })
           }).then(async res=>{
           }).catch(err=>{
